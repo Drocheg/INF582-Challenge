1b04a8e8f14891008e1b5e98236385deaec588f3
Droche <ddegrimaudetderocheb@itba.edu.ar>
2018-03-11 00:17:05 +0100

    comments graph of words and idf

--- code/feature_engineering.py
+++ code/feature_engineering.py
@@ -121,8 +121,7 @@ def feature_engineering(information_set, IDs, node_info, stemmer, stpwds, g, pai
         num_references_source.append(degrees[index_source])
         num_references_target.append(degrees[index_target])
         num_common_neighbors.append(len(neighbors_list[index_source].intersection(neighbors_list[index_target])))
-
-        num_keywords_graph_of_words.append(len(set(keywords_graph_of_words(source_abstract)).intersection(set(keywords_graph_of_words(target_abstract)))))
+        #num_keywords_graph_of_words.append(len(set(keywords_graph_of_words(source_abstract)).intersection(set(keywords_graph_of_words(target_abstract)))))
        # print pairwise_similarity.shape
      #   pairwise_similarity_number.append(pairwise_similarity[index_source, index_target])
 
@@ -153,7 +152,7 @@ def feature_engineering(information_set, IDs, node_info, stemmer, stpwds, g, pai
     list_of_features.append(num_references_target)
     list_of_features.append(num_common_neighbors)
     list_of_features.append(avg_number_citations_of_authors)
-    list_of_features.append(num_keywords_graph_of_words)
+    #list_of_features.append(num_keywords_graph_of_words)
     #list_of_features.append(pairwise_similarity_number)
     # convert list of lists into array
     # documents as rows, unique words as columns (i.e., example as rows, features as columns)
